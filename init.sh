b3106a2ff6491bb03b2d608e83176e0c2413bb8a
Karl Hans Laurits <168311564+rejoice4156@users.noreply.github.com>
2024-09-29 12:33:24 +0300

    Add missing fi statements to if blocks
    Also fix typos in if blocks for .bashrc.d/aliases.bashrc and
    .bashrc.d/variables.bashrc.

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eoux pipefail
 
-
 echo "Starting script..."
 
 # .bashrc
@@ -9,6 +8,7 @@ echo "Checking for $HOME/.bashrc..."
 if [ -e $HOME/.bashrc ]; then
     echo "Backing up $HOME/.bashrc to $HOME/.bashrc.bak..."
     cp $HOME/.bashrc $HOME/.bashrc.bak
+fi
 echo "Copying over new .bashrc to $HOME/.bashrc..."
 cp -i ./.bashrc $HOME/.bashrc
 
@@ -16,17 +16,19 @@ cp -i ./.bashrc $HOME/.bashrc
 echo "Creating $HOME/.bashrc.d if not already exists..."
 mkdir -p $HOME/.bashrc.d
 echo "Checking for $HOME/.bashrc.d/variables.bashrc..."
-if [ -e $HOME/.bashrc/variables.bashrc ]; then
+if [ -e $HOME/.bashrc.d/variables.bashrc ]; then
     echo "Backing up $HOME/.bashrc.d/variables.bashrc to $HOME/.bashrc.d/variables.bashrc.bak..."
     cp $HOME/.bashrc.d/variables.bashrc $HOME/.bashrc.d/variables.bashrc.bak
+fi
 echo "Copying over new .bashrc.d/variables.bashrc to $HOME/.bashrc.d/variables.bashrc..."
 cp -i ./.bashrc.d/variables.bashrc $HOME/.bashrc.d/variables.bashrc
 
 # .bashrc.d/aliases.bashrc
 echo "Checking for $HOME/.bashrc.d/aliases.bashrc..."
-if [ -e $HOME/.bashrc/aliases.bashrc ]; then
+if [ -e $HOME/.bashrc.d/aliases.bashrc ]; then
     echo "Backing up $HOME/.bashrc.d/aliases.bashrc to $HOME/.bashrc.d/aliases.bashrc.bak..."
     cp $HOME/.bashrc.d/aliases.bashrc $HOME/.bashrc.d/aliases.bashrc.bak
+fi
 echo "Copying over new .bashrc.d/aliases.bashrc to $HOME/.bashrc.d/aliases.bashrc..."
 cp -i ./.bashrc.d/aliases.bashrc $HOME/.bashrc.d/aliases.bashrc
 
@@ -35,6 +37,7 @@ echo "Checking for $HOME/.gitconfig..."
 if [ -e $HOME/.gitconfig ]; then
     echo "Backing up $HOME/.gitconfig to $HOME/.gitconfig.bak..."
     cp $HOME/.gitconfig $HOME/.gitconfig.bak
+fi
 echo "Copying over new .gitconfig to $HOME/.gitconfig..."
 cp -i ./.gitconfig $HOME/.gitconfig
 
@@ -43,6 +46,7 @@ echo "Checking for $HOME/.vimrc..."
 if [ -e $HOME/.vimrc ]; then
     echo "Backing up $HOME/.vimrc to $HOME/.vimrc.bak..."
     cp $HOME/.vimrc $HOME/.vimrc.bak
+fi
 echo "Copying over new .vimrc to $HOME/.vimrc..."
 cp -i ./.vimrc $HOME/.vimrc
 
@@ -53,8 +57,8 @@ echo "Checking for $HOME/.ssh/allowed_signers..."
 if [ -e $HOME/.ssh/allowed_signers ]; then
     echo "Backing up $HOME/.ssh/allowed_signers to $HOME/.ssh/allowed_signers.bak..."
     cp $HOME/.ssh/allowed_signers $HOME/.ssh/allowed_signers.bak
+fi
 echo "Copying over new .ssh/allowed_signers to $HOME/.ssh/allowed_signers..."
 cp -i ./.ssh/allowed_signers $HOME/.ssh/allowed_signers
 
 echo "Ending script..."
-
