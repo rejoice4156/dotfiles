961f983cb39598bae55f03a6de55641b305987ab
Karl Hans Laurits <168311564+rejoice4156@users.noreply.github.com>
2024-12-28 00:32:11 +0200

    Update fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path /opt/homebrew/bin/
+    fish_add_path ~/.orbstack/bin
     set -g fish_key_bindings fish_vi_key_bindings
     set -g fish_greeting
     op completion fish | source
---
+++ private_dot_config/private_fish/functions/po.fish
@@ -0,0 +1,4 @@
+function po --wraps=poetry --description 'alias po poetry'
+  poetry $argv
+        
+end
